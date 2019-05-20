f0c80c277b886552cac7e14143e591f7518cc3cd
ocularminds <mail.festus@gmail.com>
2019-05-20 19:43:35 +0100

    add interface for card issuance with Custom response object

---
+++ DexxisCardIssuance.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using RestEase;
+
+namespace dexxis.client
+{
+
+    [Header("User-Agent", "RestEase")]
+    public interface DexxisCardIssuance
+    {
+
+        [Post("isServerAvailable")]
+        Task<Boolean> IsServerAvailableAsync([Body] Dictionary<String, object> request);
+
+        [Post("createCardRequest")]
+        Task<Int32> CreateCardRequestAsync([Body] Dictionary<String, object> request);
+
+        [Post("CancelCardRequest")]
+        Task<List<Int32>> CancelCardRequestAsync([Body] Dictionary<String, object> request);
+
+        [Post("changeCardRequestSatellites")]
+        Task<List<Int32>> ChangeCardRequestSatellitesAsync([Body] Dictionary<String, object> request);
+
+        /**
+         *
+         * @param request
+         * @return
+         * @throws IOException
+         * @throws SOAPException
+         * @throws TransformerException
+         * @throws ParserConfigurationException
+         * @throws Exception
+         */
+        [Post("getCardRequest")]
+        Task<List<String>> GetCardRequestAsync([Body] Dictionary<String, object> request);
+
+        [Post("getCardRequestStatus")]
+        Task<List<String>> GetCardRequestStatusAsync([Body] Dictionary<String, object> request);
+
+        [Post("getPEDsFromBranchRequest")]
+        Task<Dictionary<string, object>> GetPEDsFromBranchRequestAsync([Body] Dictionary<String, object> request);
+    }
+}
---
+++ WebServiceResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dexxis.client
+{
+    public class WebServiceResponse{
+        public string error { get; set; }
+        public string description { get; set; }
+        public object data { get; set; }
+    }
+}
