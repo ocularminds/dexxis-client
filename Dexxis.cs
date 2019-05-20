661305ecf99848c1a1b94ad7fd161797d3331435
ocularminds <mail.festus@gmail.com>
2019-05-20 19:32:35 +0100

    initial commit

---
+++ Dexxis.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace dexiss.client
+{
+    class Dexxis
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Dexxis Client 1.0");
+        }
+    }
+}
+
